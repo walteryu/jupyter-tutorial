575b5a72db81c95c19493ff08cadd48a3db8b00e
wyu <kharma@gmail.com>
2020-07-24 10:35:59 -0700

    add plot

--- modules/03_examples.py
+++ modules/03_examples.py
@@ -1,2 +1,13 @@
-# show summary statistics
-df_california['cases'].describe()
+# 03 - data plot example and please add you own too!
+
+# set plot size and title
+plt.figure(figsize=(8, 12))
+plt.title('Count Plot: Total Case Count in CA by County')
+
+# create countplot and sort categories by value
+# https://stackoverflow.com/questions/46623583/seaborn-countplot-order-categories-by-count
+sns.countplot(
+    y="county",
+    data=df_california,
+    order = df_california['county'].value_counts().index
+)
